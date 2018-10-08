41abf40c9d08ec56145c62ff7e38f258f8511ee8
Bruno Lopes <brunoleonelopes@gmail.com>
2018-10-08 22:31:40 +0100

    added comments and a legend to the plot

--- IdentityMatrixIdentifier/main.py
+++ IdentityMatrixIdentifier/main.py
@@ -19,22 +19,33 @@ model.add(Dense(units=8, activation='relu', input_dim=8))
 model.add(Dense(units=3, activation='relu'))
 model.add(Dense(units=8, activation='softmax'))
 
+# The Adam optimizer introduced in this paper: https://arxiv.org/abs/1412.6980
+# and the nadam ( Nesterov Adam Optimizer ) talked about in http://www.cs.toronto.edu/~fritz/absps/momentum.pdf
+# provided the best results compared to other optimizers that I tried and are provided by Keras
 model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
+              optimizer='nadam',
               metrics=['accuracy'])
 
 # ------------------------------------------
 # Step b.
 #    --> Training the network to learn the 8x8 identity matrix
 # ------------------------------------------
-# 10000 iterations
-
-history = np.empty((10000, 2))
+# We train using batches of 32 samples.
+# Every batch is generated at run time instead of creating a huge dataset before the training starts.
+# Training data and label data are the same due to the nature of our problem.
 print("Step b. Training the network to learn the 8x8 identity matrix...")
-for i in range(10000):
+history = np.empty((3000, 2))
+
+for i in range(3000):
     data = identity_matrix[np.random.choice(identity_matrix.shape[0], 32), :]
     history[i] = model.train_on_batch(data, data)
 
+# Making a plot to show the evolution of the model training
+# Looking at plots like this and tweaking the parameters is very important to create good models
+plt.plot(history[:, 0], label="Loss Function (Cross Entropy)")
+plt.plot(history[:, 1], label="Accuracy")
+plt.legend()
+plt.show()
 
 data = identity_matrix[np.random.choice(identity_matrix.shape[0], 100), :]
 # print(model.metrics_names)
